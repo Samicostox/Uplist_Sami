8cb2d9008f71c545d7b3babc87083067302128a1
HHazard2002 <hugo7.hazard@gmail.com>
2024-05-06 17:17:40 +0100

    updated navbar navigation

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
 import TokenService from "../../request-model/services/token.service";
+import AuthService from "../../request-model/services/auth.service";
+import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar2 = () => {
   return (
@@ -11,15 +14,45 @@ const Navbar2 = () => {
 };
 
 const Navbar = () => {
+  let navigate = useNavigate();
+  const [accountMenuOpen, setAccountMenuOpen] = useState(false);
+  const hideDropdown = () => setAccountMenuOpen(false);
+  const [token, setToken] = React.useState(TokenService.getUser());
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleLogin = () => {
+    // logout and redirect to auth
+    AuthService.logout();
+    console.log("toto");
+    window.location.href = "/auth/login";
+    console.log("tata2");
+    //navigate("/auth/login");
+  };
+  const handleSignout = () => {
+    // logout and redirect to auth
+    hideDropdown();
+    AuthService.logout();
+    console.log("toto2");
+    window.location.href = "/auth/login";
+    console.log("tatat2");
+  };
+
+  useEffect(() => {
+    setToken(TokenService.getUser());
+
+    return function cleanup() {
+      setOpenMenu(false);
+    };
+  }, [navigate]);
   return (
     <nav className="z-50 fixed left-[50%] top-8 flex w-fit -translate-x-[50%] items-center gap-6 rounded-lg border-[1px] border-neutral-700 bg-neutral-900 p-2 text-sm text-neutral-500">
       <Logo />
 
       <NavLink href={"/artists"}>Find Artists</NavLink>
-      <NavLink href={"/bookings"}>Bookings</NavLink>
-      <NavLink href={"/account"}>Account</NavLink>
+      {token && <NavLink href={"/bookings"}>Bookings</NavLink>}
+      {token && <NavLink href={"/account"}>Account</NavLink>}
 
-      <JoinButton />
+      {token && <YourBiolinkButton />}
+      {!token && <SignInButton />}
     </nav>
   );
 };
@@ -52,7 +85,7 @@ const NavLink = ({ children, href }) => {
   );
 };
 
-const JoinButton = () => {
+const YourBiolinkButton = () => {
   const [token, setToken] = React.useState(TokenService.getUser());
 
   const getBiolinkNavigation = () => {
@@ -91,6 +124,49 @@ const JoinButton = () => {
           active:scale-100`}
     >
       Your Biolink
+    </button>
+  );
+};
+
+const SignInButton = () => {
+  const [token, setToken] = React.useState(TokenService.getUser());
+
+  const getBiolinkNavigation = () => {
+    const newToken = TokenService.getUser();
+    if (!newToken) {
+      setToken(undefined);
+      return "/auth/login";
+    }
+
+    const username = newToken.username;
+
+    return "/biolink/" + username;
+  };
+
+  const handleBiolinkClick = (e) => {
+    e.preventDefault();
+    window.location.href = getBiolinkNavigation();
+  };
+  return (
+    <button
+      onClick={handleBiolinkClick}
+      className={`
+          relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
+          border-neutral-700 px-4 py-1.5 font-medium
+         text-neutral-300 transition-all duration-300
+          
+          before:absolute before:inset-0
+          before:-z-10 before:translate-y-[200%]
+          before:scale-[2.5]
+          before:rounded-[100%] before:bg-neutral-50
+          before:transition-transform before:duration-1000
+          before:content-[""]
+  
+          hover:scale-105 hover:border-neutral-50 hover:text-neutral-900
+          hover:before:translate-y-[0%]
+          active:scale-100`}
+    >
+      Sign In
     </button>
   );
 };
